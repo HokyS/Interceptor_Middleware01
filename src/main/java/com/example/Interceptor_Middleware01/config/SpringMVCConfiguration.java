7654d863f8c57d4157eece4f6669e5a40566a743
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-11-02 01:15:24 +0100

    Creating Spring MVC Configuration

---
+++ src/main/java/com/example/Interceptor_Middleware01/config/SpringMVCConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.Interceptor_Middleware01.config;
+
+import com.example.Interceptor_Middleware01.interceptors.APILoggingInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class SpringMVCConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private APILoggingInterceptor apiLoggingInterceptor;
+
+    @Autowired
+    private APILoggingInterceptor legacyInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiLoggingInterceptor);
+        registry.addInterceptor(legacyInterceptor);
+    }
+}
